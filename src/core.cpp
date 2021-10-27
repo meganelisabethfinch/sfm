4d01ac9786f17c3949783c4188b4e7e1734cd16d
meganelisabethfinch <meganelisabethfinch@gmail.com>
2021-10-27 12:21:13 +0100

    Implemented feature detection with SIFT

--- src/core.cpp
+++ src/core.cpp
@@ -3,21 +3,22 @@
 #include <iostream>
 
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/features2d.hpp>
 
 int main(int argc, char** argv) {
-    // parse inputs
-    std::string inputImageDir;
-    std::string outputDir;
+    #pragma region Parse_Command_Line
+    std::string input_image_dir;
+    std::string output_dir;
     int opt;
     
     while ((opt = getopt(argc, argv, "i:o:")) != -1) {
         switch (opt) {
             case 'i': {
-                inputImageDir = optarg;
+                input_image_dir = optarg;
                 break; 
             }
             case 'o': {
-                outputDir = optarg;
+                output_dir = optarg;
                 break;
             }
             default: {
@@ -27,19 +28,33 @@ int main(int argc, char** argv) {
     }
 
     std::vector<cv::String> fn;
-    cv::glob(inputImageDir + "/*.png", fn, false);
+    cv::glob(input_image_dir + "/*.png", fn, false);
     // TODO: fix format for *.png and *.jpg
     // TODO: error handling if glob fails/image directory is invalid
 
-    std::vector<cv::Mat> inputImages;
+    std::cout << "Opening images..." << std::endl;
+    std::vector<cv::Mat> input_images;
     for (size_t i = 0; i < fn.size(); i++) {
-        cv::Mat image = cv::imread(fn[i], 1);
-        if (image.data) {
-            inputImages.push_back(image);
-        }
+        input_images.push_back(cv::imread(fn[i], cv::IMREAD_COLOR));
+    }
+    #pragma endregion Parse_Command_Line
+
+    #pragma region Feature_Detection
+    std::cout << "Finding features..." << std::endl;
+
+    cv::Ptr<cv::SIFT> detector = cv::SIFT::create();
+    std::vector<std::vector<cv::KeyPoint>> all_keypoints;
+    std::vector<cv::Mat> all_descriptors;
+    for (size_t i = 0; i < input_images.size(); i++) {
+        std::vector<cv::KeyPoint> keypoints;
+        cv::Mat descriptors;
+
+        detector->detectAndCompute(input_images[i], cv::noArray(), keypoints, descriptors);
+        all_keypoints.push_back(keypoints);
+        all_descriptors.push_back(descriptors);
     }
 
-    // 1. detect features
+    #pragma endregion Feature_Detection
 
     // 2. feature matching
 
