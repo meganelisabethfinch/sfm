0c8d107635d9e77e178d463fb183d48f3f8740a3
meganelisabethfinch <meganelisabethfinch@gmail.com>
2021-12-18 22:40:39 +0000

    trying and failing to get google test working (2)

---
+++ tests/gtest_main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Megan Finch on 18/12/2021.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
